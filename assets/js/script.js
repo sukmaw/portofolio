6b4b4f42573a2fe9c7fc50cccf291eff49bfba7e
luthfi554 <luthfi552@gmail.com>
2018-05-12 09:38:30 +0700

    v2

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready( function(){
+	$('.sidenav').sidenav();
+	$('.parallax').parallax();
+	
+	// navsmooth
+	
+	$(".navsmooth").on('click', function(event) {
+		if(this.hash !== ""){
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash;
+			});
+		}
+	});
+} )
